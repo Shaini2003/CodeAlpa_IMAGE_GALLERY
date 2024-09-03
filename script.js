b751c810233687a038ec7030ff463052cf81fa8e
Shaini2003 <shinitharushika2020@gmail.com>
2024-09-03 21:14:04 +0530

    Second Commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const lightBoxContainer = document.createElement("div");
 //for basic area
 const lightBoxContent = document.createElement("div");
 //for images
-const lightBoxImg = document.createElement("div");
+const lightBoxImg = document.createElement("img");
 //for prev button to change images
 const lightBoxPrev = document.createElement("div");
 //for next buttton
@@ -35,3 +35,30 @@ function showLightBox(i) {
     let imageLocation = galleryItem[index - 1].children[0].getAttribute("src");
     lightBoxImg.setAttribute("src",imageLocation);
 }
+function currentImage(){
+    lightBoxContainer.style.display="block";
+    let imageIndex = parseInt(this.getAttribute("data-index"));
+    showLightBox(index=imageIndex);
+}
+for(let i=0; i<galleryItem.length;i++){
+    galleryItem[i].addEventListener("click",currentImage);
+}
+function sliderImage(i){
+    showLightBox(index += i);
+}
+function prevImage(){
+    sliderImage(-1);
+}
+function nextImage(){
+    sliderImage(1);
+}
+lightBoxPrev.addEventListener("click",prevImage);
+lightBoxNext.addEventListener("click",nextImage);
+
+function closeLightBox(){
+    if(this === event.target){
+        lightBoxContainer.style.display = "none";
+
+    }
+}
+lightBoxContainer.addEventListener("click",closeLightBox);
